09e0caaf9af8d2e0c660ed3097477e9671ec718b
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-07 20:12:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 && number % 5 == 0 
+  if number%3 && number%5 == 0 
     return "FizzBuzz"
-  elsif number % 3 == 0 
+  elsif number%3 == 0 
     return "Fizz"
-  elsif number % 5
+  elsif number%5
     return "Buzz"
   else 
     return
