6e5e861df49d3580b1670845985db781a1e4a314
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-07 20:05:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
   def fizzbuzz(number)
-    if number % 3 == 0 
-      puts "Fizz"
-    elsif number % 5 == 0 
-      puts "Buzz"
-    elsif number % 3 && number % 5 == 0 
+    if number % 3 && number % 5 == 0 
       puts "FizzBuzz"
+    elsif number % 3 == 0 
+      puts "fizz"
+    elsif number % 5
+    puts "buzz"
     end
 end 
 
