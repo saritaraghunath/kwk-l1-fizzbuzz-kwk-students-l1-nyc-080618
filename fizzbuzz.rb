072918e4486f29bcc6d1c192c8c4382c2a7d73df
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-07 19:53:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # number = gets.chomp.to_i 
-  
-  number = 6
-  if number % 3 == 0 
-    puts "Fizz"
-  elsif number % 5 == 0 
-    puts "Buzz"
-  elsif number % 3 && number % 5 == 0 
-    puts "FizzBuzz"
+  def fizzbuzz(number)
+    if number % 3 == 0 
+      puts "Fizz"
+    elsif number % 5 == 0 
+      puts "Buzz"
+    elsif number % 3 && number % 5 == 0 
+      puts "FizzBuzz"
+end 
